3de00f556d9cdc3fbca8638eb0f11d1e64f70f5b
Thomas Rößl <e11775192@student.tuwien.ac.at>
2018-06-15 16:54:13 +0200

    Fix quality issues

--- README.md
+++ README.md
@@ -3,30 +3,12 @@
 #### Table of Contents
 
 1. [Overview](#overview)
-2. [Setup - The basics of getting started with openhab2](#setup)
-    * [What openhab2 affects](#what-openhab2-affects)
 3. [Usage - Configuration options and additional functionality](#usage)
-4. [Reference - An under-the-hood peek at what the module is doing and how](#reference)
-5. [Limitations - OS compatibility, etc.](#limitations)
 
 ## Overview
 
 Installs and configures a basic openhab2 instance.
 
-## Setup
-
-### What openhab2 affects
-
-* Ensures that openjdk 8 is installed.
-
 ## Usage
 
-The module only does a bisc installation.
-
-## Reference
-
-openhab2 requires java, so openjdk8 is installed.
-
-## Limitations
-
-Only tested with ubuntu and centos.
+The module only does a basic installation.
--- manifests/init.pp
+++ manifests/init.pp
@@ -12,46 +12,49 @@
 #
 class openhab2 {
 
-  $javapackage = $osfamily ? {
+  $javapackage = $::osfamily ? {
     'redhat' => 'java-1.8.0-openjdk',
-    'debian' => 'openjdk-8-jre',
+    'debian' => 'openjdk-8-jre'
   }
 
-  package { $javapackage:
-    ensure => 'installed',
+  class { 'java' :
+    package => $javapackage,
   }
 
-  case $osfamily {
+  case $::osfamily {
     'redhat': {
       file { 'repo-file':
-        ensure => 'present',
-        path => '/etc/yum.repos.d/openhab.repo',
+        ensure  => 'present',
+        path    => '/etc/yum.repos.d/openhab.repo',
         content => file('openhab2/stable.repo')
       }
       package { 'openhab2':
-        ensure => 'installed',
+        ensure  => 'installed',
         require => File['repo-file']
       }
     }
     'debian': {
       exec { 'aptget-addkey':
         command => "wget -qO - 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab' | sudo apt-key add -",
-        path => '/bin:/usr/bin'
+        path    => '/bin:/usr/bin'
       }
       exec { 'aptget-addrepo':
         command =>"echo 'deb https://dl.bintray.com/openhab/apt-repo2 stable main' | sudo tee /etc/apt/sources.list.d/openhab2.list",
-        path => '/bin:/usr/bin',
+        path    => '/bin:/usr/bin',
         require => Exec['aptget-addkey']
       }
       exec { 'aptget-update':
         command => "sudo apt-get update",
-        path => '/usr/bin',
+        path    => '/usr/bin',
         require => Exec['aptget-addrepo']
       }
       package { 'openhab2':
-        ensure => 'installed',
+        ensure  => 'installed',
         require => Exec['aptget-update']
       }
+    }
+    default: {
+      fail("Module ${module_name} is not supported on ${::operatingsystem}")
     }
   }
 
